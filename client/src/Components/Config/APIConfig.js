0cb6bd6fb7f64c63f5cd4807d5c8d48920c27e89
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-02-08 19:35:56 +0530

    updates

--- client/src/Components/Config/APIConfig.js
+++ client/src/Components/Config/APIConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:8000'
-  baseURL:'https://shivani-myntra-clone.onrender.com'
+  baseURL:'https://shivaniecom.onrender.com'
 });
 
 export default api;
